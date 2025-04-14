05a88165380c2191e29685b6735398d9168f6a46
BenjaminGott <benjamin.gottrant@gmail.com>
2025-04-14 11:39:39 +0200

    Title page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Benjamin GOTTRANT',
+  description: 'Portfolio benjamin gottrant',
   generator: 'v0.dev',
 }
 
